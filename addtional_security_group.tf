b2d3a8b3fcf229dc8cbeb5da6e4755b70d697b69
aluc <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2021-06-18 01:59:44 +0900

    fix: app_ecs_allow_outbound only allow tcp protocol

--- addtional_security_group.tf
+++ addtional_security_group.tf
@@ -15,7 +15,7 @@ resource "aws_security_group_rule" "app_ecs_allow_outbound" {
   type              = "egress"
   from_port         = 443
   to_port           = 443
-  protocol          = "-1"
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
